541430438c6837571e2b2a5264be14ef3ede7664
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2022-11-06 19:36:06 +0100

    empezando el js

---
+++ index.js
@@ -0,0 +1,13 @@
+//https://www.youtube.com/watch?v=7YDagj3cVAk&ab_channel=Programaci%C3%B3nAccesible
+
+
+const displayValorAnterior =document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores =document.querySelectorAll('.operador');
+
+
+document.addEventListener("click", function(){
+ let numClick = document.getElementsByClassName('numero');
+    console.log(numClick);
+});
